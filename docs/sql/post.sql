987783ba1451b8660a49e2ab3e3a446ecc60a5ee
fakeowl1 <ukolov.roman@lll.kpi.ua>
2025-11-09 14:36:31 +0200

    sql: post type change

--- docs/sql/post.sql
+++ docs/sql/post.sql
@@ -2,7 +2,7 @@
 CREATE TABLE IF NOT EXISTS Post (
     id SERIAL PRIMARY KEY,
     creator_id INT REFERENCES Member (id),
-    title VARCHAR(32),
+    title VARCHAR(64),
     text VARCHAR(1024),
     likes INT DEFAULT 0 CHECK (likes >= 0),
     dislikes INT DEFAULT 0 CHECK (dislikes >= 0),
@@ -16,7 +16,6 @@ VALUES (1, 'Google make Android delevelopment private', 'Google is planning a ma
 
 Google has confirmed to Android Authority that all Android development work going forward will take place in Google’s internal branch. This is a shift from the way Google has worked on Android in the past, which featured frequent updates to the public AOSP branch. Anyone can access AOSP, but the internal branches are only available to Google and companies with a Google Mobile Services (GMS) license, like Samsung, Motorola, and others.');
 
--- User delete the post
+-- User delete a post
 DELETE FROM Post WHERE id = 1;
 
--- User edit the post
