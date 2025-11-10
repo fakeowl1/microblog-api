df7015e2aa1b4ad670c90e137986cfc4bd817394
fakeowl1 <ukolov.roman@lll.kpi.ua>
2025-11-10 18:36:13 +0200

    sql: add tag table and operations

--- docs/sql/post.sql
+++ docs/sql/post.sql
@@ -10,27 +10,27 @@ CREATE TABLE IF NOT EXISTS Post (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
--- User creates the post
+-- User creates post
 INSERT INTO Post(creator_id, title, text)
 VALUES (1, 'Google make Android delevelopment private', 'Google is planning a major change to the way it develops new versions of the Android operating system. Since the beginning, large swaths of the software have been developed in public-facing channels, but that will no longer be the case. This does not mean Android is shedding its open source roots, but the process won’t be as transparent.
 
 Google has confirmed to Android Authority that all Android development work going forward will take place in Google’s internal branch. This is a shift from the way Google has worked on Android in the past, which featured frequent updates to the public AOSP branch. Anyone can access AOSP, but the internal branches are only available to Google and companies with a Google Mobile Services (GMS) license, like Samsung, Motorola, and others.');
 
--- User edit a post
+-- User edit post
 UPDATE Post SET title = 'New title', text = 'New text' WHERE id = 1;
 
--- User view a post
+-- User views post
 SELECT 
   p.id as post_id,
   m.username as creator, 
   title, text, 
   likes, dislikes, views, 
-  created_at 
+  created_at
 FROM Post p 
 JOIN Member m ON m.id = p.creator_id 
 WHERE p.id = 1;
 
--- User delete a post
+-- User deletes a post
 DELETE FROM Post WHERE id = 1;
 
 -- User likes a post
@@ -39,5 +39,22 @@ UPDATE Post SET likes = likes + 1 WHERE id = 1;
 -- User dislikes a post
 UPDATE Post SET dislikes = dislikes + 1 WHERE id = 1; 
 
--- User open post
+-- User opens post
 UPDATE Post SET views = views + 1 WHERE id = 1;
+
+-- Create table for tag
+CREATE TABLE IF NOT EXISTS Tag (
+    id SERIAL PRIMARY KEY,
+    post_id INT REFERENCES Post (id) ON DELETE CASCADE,
+    name VARCHAR(32)
+);
+
+-- User adds tag
+INSERT INTO Tag(post_id, name)
+VALUES (1, 'Evil Corp');
+
+-- User deletes tag
+DELETE FROM Tag WHERE id = 1;
+
+-- User views tags for post
+SELECT * FROM Tag WHERE post_id = 1;
