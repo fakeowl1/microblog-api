a5c17005709767463804240b7a95b307f4b45519
fakeowl1 <ukolov.roman@lll.kpi.ua>
2025-11-11 13:47:47 +0200

    sql: change order of operations in posts.sql

--- docs/sql/post.sql
+++ docs/sql/post.sql
@@ -66,12 +66,12 @@ CREATE TABLE IF NOT EXISTS PostMedia (
     media_url VARCHAR(128)
 );
 
--- Get media for a post
-SELECT id, media_url FROM PostMedia WHERE post_id = 1;
-
 -- Add media to post
 INSERT INTO PostMedia (post_id, media_url)
 VALUES (1, 'https://example.com/media/first_post.jpg');
+
+-- Get media for a post
+SELECT id, media_url FROM PostMedia WHERE post_id = 1;
 
 -- Remove media from post
 DELETE FROM PostMedia WHERE id = 1;
