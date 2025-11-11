a5acaee12a50ee04c8372c88e4577cb15510729a
fakeowl1 <ukolov.roman@lll.kpi.ua>
2025-11-11 14:01:40 +0200

    sql: change commentaries & improve syntax

--- docs/sql/private-message.sql
+++ docs/sql/private-message.sql
@@ -3,26 +3,26 @@ CREATE TYPE MESSAGE_STATUS AS ENUM('readed', 'unreaded');
 
 CREATE TABLE IF NOT EXISTS PrivateMessage (
     id SERIAL PRIMARY KEY,
-    sender_id INTEGER references Member (id),
-    receiver_id INTEGER references Member (id),
+    sender_id INTEGER REFERENCES Member (id),
+    receiver_id INTEGER REFERENCES Member (id),
     text VARCHAR(512),
     status MESSAGE_STATUS NOT NULL,
     media_url VARCHAR(128),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP 
 );
 
--- Send a private message
+-- User sends a private message
 INSERT INTO PrivateMessage (sender_id, receiver_id, text, status)
 VALUES (1, 2, 'Привіт! Як справи?', 'unreaded');
 
--- Edit message
+-- User edits message
 UPDATE PrivateMessage SET text = 'Привіт! Як твої справи?' WHERE id = 1;
 
--- Mark message as read
+-- User mark a message as read
 UPDATE PrivateMessage SET status = 'readed'
 WHERE id = 1 AND receiver_id = 2;
 
--- View messages from a user
+-- User view messages from a user
 SELECT 
     pm.id,
     sender.username AS from_user,
@@ -35,5 +35,5 @@ JOIN Member sender ON pm.sender_id = sender.id
 WHERE pm.receiver_id = 2
 ORDER BY pm.created_at DESC;
 
--- Delete message
+-- User deletes a message
 DELETE FROM PrivateMessage WHERE id = 1;
