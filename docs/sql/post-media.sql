e7e6b3b8eeeb5735ab1d70f5fc950a32af43e061
KaterynaHl <glebakata50@gmail.com>
2025-11-11 04:25:27 +0200

    sql: add full OLTP for PostMedia

--- docs/sql/post-media.sql
+++ docs/sql/post-media.sql
@@ -4,3 +4,16 @@ CREATE TABLE IF NOT EXISTS PostMedia (
     post_id INT REFERENCES Post (id),
     media_url VARCHAR(128)
 );
+
+-- Get media for a post
+SELECT id, media_url FROM PostMedia WHERE post_id = 1;
+
+-- Add media to post
+INSERT INTO PostMedia (post_id, media_url)
+VALUES (1, 'https://example.com/media/first_post.jpg');
+
+-- Remove media from post
+DELETE FROM PostMedia WHERE id = 1;
+
+-- Update media URL
+UPDATE PostMedia SET media_url = 'https://example.com/media/updated_post.jpg' WHERE id = 1;
