b90c43c3a0ef3c0b1725d3e047fa699504e24f85
KaterynaHl <glebakata50@gmail.com>
2025-11-30 05:02:34 +0200

    docs(olap.sql): add queries with subqueries in SELECT, WHERE, and HAVING

--- db/Lab4/olap.sql
+++ db/Lab4/olap.sql
@@ -13,7 +13,7 @@ GROUP BY m.id
 ORDER BY m.id ASC;
 
 
-SELECT COUNT(DISTINC p.id) as number_of_posts
+SELECT COUNT(DISTINCT p.id) as number_of_posts
 FROM Post p
 INNER JOIN Tag t ON t.post_id = p.id;
 
@@ -34,3 +34,27 @@ SELECT m.username, p.id AS post_id, p.title
 FROM Post p
 RIGHT JOIN Member m ON p.creator_id = m.id
 ORDER BY m.username;
+
+
+SELECT m.username,
+(SELECT COUNT(*)
+FROM Friendship f
+WHERE (f.user_id = m.id OR f.friended_id = m.id)
+AND f.status = 'accepted') AS friends_count
+FROM Member m
+ORDER BY friends_count DESC;
+
+
+SELECT username
+FROM Member
+WHERE id IN 
+(SELECT creator_id 
+FROM Comment
+WHERE LENGTH(text) > 100);
+
+
+SELECT m.username, AVG(p.likes) AS avg_likes
+FROM Member m
+JOIN Post p ON p.creator_id = m.id
+GROUP BY m.username
+HAVING AVG(p.likes) > 10;
